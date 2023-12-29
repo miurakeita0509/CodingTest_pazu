ded9e63eb9b452639d78924a24cd89bfff5737d4
miurakeita0509 <16eo00127@gmail.com>
2023-12-29 23:07:13 +0900

    テストコードの作成 テスト観点 ・isSequential関数正常系入力にて、正しくtrueまたはfalseが返ってくるか

--- src/random-entry.test.ts
+++ src/random-entry.test.ts
@@ -1,8 +1,8 @@
-import { createRandomNum, findSequences, columnNumberToName } from './random-entry';
+import { createRandomNum, findSequences, columnNumberToName,isSequential } from './random-entry';
 
 /**
  * createRandomNum関数のテスト
- * 5行6列が作成されるかどうか
+ * 5行6列が作成されるかどうか。
  */
 describe('createRandomNum', () => {
   it('should return an array', () => {
@@ -16,7 +16,57 @@ describe('createRandomNum', () => {
   });
 });
 
+/**
+ * isSequential関数のテスト
+ * false、trueが返ってくるか。
+ */
+describe('isSequential', () => {
+  it('should return false for a new number', () => {
+    const testGrid = [
+      [1, 2, 3, 4, 5, 6],
+      [6, 5, 4, 3, 2, 1],
+      [1, 2, 3, 4, 5, 6],
+      [6, 5, 4, 3, 2, 1],
+      [1, 2, 1, 1, 1, 4]
+    ];
+    const i = 1;
+    const j = 1;
+    const num = 5;
+    const result = isSequential(testGrid, i, j, num);
+    expect(result).toBe(false);
+  });
 
+  it('should return true for a new number', () => {
+    const testGrid = [
+      [1, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1],
+    ];
+    const i = 0;
+    const j = 2;
+    const num = 1;
+    const result = isSequential(testGrid, i, j, num);
+    expect(result).toBe(true);
+  });
+
+  it('should return true for a new number', () => {
+    const testGrid = [
+      [1, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1],
+    ];
+    const i = 2;
+    const j = 1;
+    const num = 1;
+    const result = isSequential(testGrid, i, j, num);
+    expect(result).toBe(true);
+  });
+});
+
+/**
+ * columnNumberToNam関数のテスト
+ * アルファベットA-Fが抜き出せるかどうか。
+ */
 describe('columnNumberToNam', () => {
   it('should return "A" for 1', () => {
     expect(columnNumberToName(1)).toBe('A');
@@ -40,7 +90,7 @@ describe('columnNumberToNam', () => {
 
 /**
  * findSequences関数のテスト
- * 連続する値が正しくカウントできているかどうか
+ * 連続する値が正しくカウントできているかどうか。
  */
 describe('findSequences', () => {
   it('should find sequences correctly 1', () => {
@@ -49,7 +99,7 @@ describe('findSequences', () => {
       [6, 5, 4, 3, 2, 1],
       [1, 2, 3, 4, 5, 6],
       [6, 5, 4, 3, 2, 1],
-      [1, 2, 1, 1, 1, 4]  // 最下段にて1が連続
+      [1, 2, 1, 1, 1, 4]  // 最下段にて1が連続。
     ];
     const rows = 5;
     const cols = 6;
@@ -59,11 +109,11 @@ describe('findSequences', () => {
 
   it('should find sequences correctly 3', () => {
     const testgrid = [
-      [1, 1, 1, 2, 3, 4],  // 3つの1が連続
+      [1, 1, 1, 2, 3, 4],  // 3つの1が連続。
       [2, 2, 3, 4, 5, 6],
-      [3, 4, 5, 6, 6, 6],  // 3つの6が連続
+      [3, 4, 5, 6, 6, 6],  // 3つの6が連続。
       [4, 5, 6, 1, 2, 3],
-      [5, 6, 2, 2, 2, 2],  // 4つの2が連続
+      [5, 6, 2, 2, 2, 2],  // 4つの2が連続。
     ];
     const rows = 5;
     const cols = 6;
@@ -77,7 +127,7 @@ describe('findSequences', () => {
       [1, 2, 3, 5, 5, 4],
       [1, 3, 3, 4, 3, 6],
       [2, 2, 4, 4, 5, 6],  // シーケンスが行や列の最後にある場合に、
-      [2, 2, 3, 6, 6, 6]   //それを正しく検出できているかどうかの確認
+      [2, 2, 3, 6, 6, 6]   //それを正しく検出できているかどうかの確認。
     ];
     const rows = 5;
     const cols = 6;
@@ -91,7 +141,7 @@ describe('findSequences', () => {
       [2, 2, 2, 2, 2, 2],
       [3, 3, 3, 3, 3, 3],  
       [4, 4, 4, 4, 4, 4],
-      [5, 5, 5, 5, 5, 5]  // 各行で3つの1, 2, 3, 4, 5, 6が連続
+      [5, 5, 5, 5, 5, 5]  // 各行で3つの1, 2, 3, 4, 5, 6が連続。
     ];
     const rows = 5;
     const cols = 6;
@@ -105,7 +155,7 @@ describe('findSequences', () => {
       [1, 2, 3, 4, 5, 6],
       [1, 2, 3, 4, 5, 6],
       [1, 2, 3, 4, 5, 6],
-      [1, 2, 3, 4, 5, 6]  // 各列で3つの1, 2, 3, 4, 5, 6が連続
+      [1, 2, 3, 4, 5, 6]  // 各列で3つの1, 2, 3, 4, 5, 6が連続。
     ];
     const rows = 5;
     const cols = 6;
@@ -119,7 +169,7 @@ describe('findSequences', () => {
       [1, 1, 1, 1, 1, 1],
       [1, 1, 1, 1, 1, 1],
       [1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1]  // 1が連続
+      [1, 1, 1, 1, 1, 1]  // 1が連続。
     ];
     const rows = 5;
     const cols = 6;
@@ -133,7 +183,7 @@ describe('findSequences', () => {
       [2, 2, 2, 2, 2, 2],
       [2, 2, 2, 2, 2, 2],
       [2, 2, 2, 2, 2, 2],
-      [2, 2, 2, 2, 2, 2]  // 2が連続
+      [2, 2, 2, 2, 2, 2]  // 2が連続。
     ];
     const rows = 5;
     const cols = 6;
@@ -147,7 +197,7 @@ describe('findSequences', () => {
       [3, 3, 3, 3, 3, 3],
       [3, 3, 3, 3, 3, 3],
       [3, 3, 3, 3, 3, 3],
-      [3, 3, 3, 3, 3, 3]  // 3が連続
+      [3, 3, 3, 3, 3, 3]  // 3が連続。
     ];
     const rows = 5;
     const cols = 6;
@@ -161,7 +211,7 @@ describe('findSequences', () => {
       [4, 4, 4, 4, 4, 4],
       [4, 4, 4, 4, 4, 4],
       [4, 4, 4, 4, 4, 4],
-      [4, 4, 4, 4, 4, 4]  // 4が連続
+      [4, 4, 4, 4, 4, 4]  // 4が連続。
     ];
     const rows = 5;
     const cols = 6;
@@ -175,7 +225,7 @@ describe('findSequences', () => {
       [5, 5, 5, 5, 5, 5],
       [5, 5, 5, 5, 5, 5],
       [5, 5, 5, 5, 5, 5],
-      [5, 5, 5, 5, 5, 5]  // 5が連続
+      [5, 5, 5, 5, 5, 5]  // 5が連続。
     ];
     const rows = 5;
     const cols = 6;
@@ -189,7 +239,7 @@ describe('findSequences', () => {
       [6, 6, 6, 6, 6, 6],
       [6, 6, 6, 6, 6, 6],
       [6, 6, 6, 6, 6, 6],
-      [6, 6, 6, 6, 6, 6]  // 6が連続
+      [6, 6, 6, 6, 6, 6]  // 6が連続。
     ];
     const rows = 5;
     const cols = 6;
